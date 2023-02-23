e369c787ea84552ca721e742d9f0e504e46dd83c
sanghee Shin <113161106+shinsanghee@users.noreply.github.com>
2023-02-23 17:16:10 +0900

    Create Var.java

---
+++ Var.java
@@ -0,0 +1,13 @@
+package com.mycompany.myapp;
+
+public class Var {
+	public static void main(String[] args) {
+		int price = 200;
+		String name= "kitae";
+		double pi = 3.14;
+
+		System.out.print("price = " + (price + 1000));
+		System.out.print("name = " + name);
+		System.out.print("pi = " + pi*10);
+	}
+}
